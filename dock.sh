aebd79efd9246504554ee6a96f3f086d7f74dacb
BrooksMoney <52867637+brooksmillioner@users.noreply.github.com>
2024-05-15 19:06:34 +0300

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade -y
+echo "Update you system"
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install git-all -y && sudo apt full-upgrade -y
+sleep 5
+
+echo "Installing docker or docker compose"
 
 
 #Logo
